cb1ae0b10eb46cbc7bb3fa73051dfcdd4888e657
felixottander <felix.ottander@yahoo.com>
2017-04-03 15:14:04 +0200

    Adds routes for the shoppingcart. Adds checkout button in view. Adds controller for shoppingcart and modifies the cart items controller.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :shopping_cart_items, only: [:create], as: :items
 
+  resources :shopping_carts, only: [:show], as: :cart
+
   resources :restaurants, only: [:show, :new, :create] do
     resources :menus, only: [:show]
   end
