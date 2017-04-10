d97382f7d003265615ebdf3b9560ea099c41ee51
johan <johan.schantz@telia.com>
2017-04-10 16:26:07 +0200

    deleted some rspec configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,12 +14,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryGirl::Syntax::Methods
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
 end
 
 Shoulda::Matchers.configure do |config|
