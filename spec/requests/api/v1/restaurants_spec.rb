4624238cac0e53c8c592f88d804cbbe1257fab58
felixottander <felix.ottander@yahoo.com>
2017-04-11 11:22:34 +0200

    Modifies the request spec.

--- spec/requests/api/v1/restaurants_spec.rb
+++ spec/requests/api/v1/restaurants_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RestaurantsController, type: :request do
-  let(:restaurant) { create(:restaurant) }
 
   describe 'GET /v1/restaurants' do
     it 'should return all the restaurants' do
-      create(:restaurant)
+      restaurant = create(:restaurant)
       get '/api/v1/restaurants'
-      expect(response.status).to eq 200
+
       expected_response = {
-        'entries' => {
+        'entries' => [{
           'id' => restaurant.id, 'name' => restaurant.name, 'street' => restaurant.street,
           'zip_code' => restaurant.zip_code, 'city' => restaurant.city, 'food_style' => restaurant.food_style,
+          'created_at' => restaurant.created_at.strftime("%FT%T.%LZ"), 'updated_at' => restaurant.updated_at.strftime("%FT%T.%LZ"),
           'description' => restaurant.description
-        }      }
+        }]     }
 
       expect(response_json).to eq expected_response
+      expect(response.status).to eq 200
     end
   end
 end
