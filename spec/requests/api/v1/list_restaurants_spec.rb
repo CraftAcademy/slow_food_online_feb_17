84654d30d7548103ef903658843502432d1a592e
felixottander <felix.ottander@yahoo.com>
2017-04-11 08:42:14 +0200

    Refactoring of the spec.

--- spec/requests/api/v1/list_restaurants_spec.rb
+++ spec/requests/api/v1/list_restaurants_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe Api::V1::ListRestaurantsController, type: :request do
     it 'should return all the restaurants' do
       create(:restaurant)
       get '/api/v1/list_restaurants'
-      json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(json_response['entries'].first['name']).to eq "Nisses"
+      expect(response_json['entries'].first['name']).to eq "Nisses"
     end
   end
 end
