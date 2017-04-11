16617a01027e9fbcfb0fb2c74c21636f80ce17a5
felixottander <felix.ottander@yahoo.com>
2017-04-11 09:49:47 +0200

    Updates the restaurants factory and the response spec.

--- spec/requests/api/v1/restaurants_spec.rb
+++ spec/requests/api/v1/restaurants_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RestaurantsController, type: :request do
-  let(:restaurant) { FactoryGirl.create(:restaurant) }
+  let(:restaurant) { create(:restaurant) }
 
   describe 'GET /v1/restaurants' do
     it 'should return all the restaurants' do
@@ -10,7 +10,7 @@ RSpec.describe Api::V1::RestaurantsController, type: :request do
       expect(response.status).to eq 200
       expected_response = {
         'entries' => {
-          'id' => restaurant.id, 'Nisses' => restaurant.name, 'street' => restaurant.street,
+          'id' => restaurant.id, 'name' => restaurant.name, 'street' => restaurant.street,
           'zip_code' => restaurant.zip_code, 'city' => restaurant.city, 'food_style' => restaurant.food_style,
           'description' => restaurant.description
         }      }
