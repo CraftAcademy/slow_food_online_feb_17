75c389242c4849fbba92d523426e51c5258ef5b2
casperjureskog <lcasper@mac.com>
2017-04-13 09:58:43 +0200

    moves update rating to model

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,17 +1,9 @@
 class RatingsController < ApplicationController
   before_action :get_shopping_cart
 
-  def rate
+  def update
     @rating = Rating.find_by(restaurant_id: params[:id])
-    if params[:rating]
-      count = @rating.counter + 1
-      if params[:rating][:rating].to_f > @rating.rating
-        rat = (params[:rating][:rating].to_f / count.to_f ) + @rating.rating
-      else
-        rat = @rating.rating - (params[:rating][:rating].to_f / count.to_f )
-      end
-      @rating.update(rating: rat , counter: count.to_i)
-    end
+    @rating.update_rating(params)
     render "shopping_carts/complete"
   end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,4 +5,19 @@ class Rating < ApplicationRecord
   def self.rating_range
     ['1','2','3','4','5']
   end
+
+
+
+
+  def update_rating(params)
+    if params[:rating]
+      count = self.counter + 1
+      if params[:rating][:rating].to_f > self.rating
+        rat = (params[:rating][:rating].to_f / count.to_f ) + self.rating
+      else
+        rat = self.rating - (params[:rating][:rating].to_f / count.to_f )
+      end
+      self.update(rating: rat , counter: count.to_i)
+    end
+  end
 end
